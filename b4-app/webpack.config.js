a6130787e8da7220a798a4882ea11cd8e01ee2c5
moonbury <moonbury@hotmail.com>
2018-08-02 09:12:47 +0000

    add a web UI

---
+++ b4-app/webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+const path = require('path');
+const distDir = path.resolve(__dirname, 'dist');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: distDir,
+  },
+  devServer: {
+    // .. rest of devserver options
+
+    host: '0.0.0.0',
+    port: 60800,
+    contentBase: distDir,
+    disableHostCheck: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Better Book Bundle Builder',
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader','css-loader']
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit:10000',
+    }]
+  }
+};
