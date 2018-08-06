21c45bcf144f25c6672b02e9e2b254d16e5826a7
moonbury <moonbury@hotmail.com>
2018-08-06 07:00:12 +0000

    add files

---
+++ web-services/hello/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.get('/hello/:name',(req,res)=> {
+  res.status(200).json({'hello':req.params.name});
+});
+
+app.get('/',(req,res)=> {
+  res.status(200).json({'msg':'Welcome!'});
+});
+app.listen(60701,()=>console.log('ready.'));
