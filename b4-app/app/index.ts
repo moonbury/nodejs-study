7b21a40548015455c3881d490bc3c823bef5a998
moonbury <moonbury@hotmail.com>
2018-08-06 03:40:13 +0000

    finishing the app

--- b4-app/app/index.ts
+++ b4-app/app/index.ts
@@ -8,6 +8,11 @@ document.body.innerHTML = templates.main();
 const mainElement = document.body.querySelector('.b4-main');
 const alertsElement = document.body.querySelector('.b4-alerts');
 
+const showAlert = (message, type = 'danger') => {
+  const html = templates.alert({ type, message });
+  alertsElement.insertAdjacentHTML('beforeend', html);
+};
+
 /*
 mainElement.innerHTML = templates.welcome();
 alertsElement.innerHTML = templates.alert({
@@ -15,21 +20,51 @@ alertsElement.innerHTML = templates.alert({
   message: 'Handlebars is working!',
 });
 */
+
+
+/* add a bundle */
+const addBundle = async (name) => {
+  try {
+    const bundles = await getBundles();
+
+    const url = `/api/bundle?name=${encodeURIComponent(name)}`;
+    const res = await fetch(url, { method: 'POST' });
+    const resBody = await res.json();
+
+    bundles.push({ id: resBody._id, name });
+    listBundles(bundles);
+
+    showAlert(`Bundle ${name} created!`, 'success');
+
+  } catch (err) {
+    showAlert(err);
+  }
+}
+
 /* get all the bundles */
-const getBundles = async() => {
+const getBundles = async () => {
   console.log("in getBundles");
 
   const esRes = await fetch('/es/b4/bundle/_search?size=1000');
   const esResBody = await esRes.json();
   console.log(esResBody);
-  return esResBody.hits.hits.map(hit=> ({
+  return esResBody.hits.hits.map(hit => ({
     id: hit._id,
     name: hit._source.name,
   }));
 };
 
 const listBundles = bundles => {
-  mainElement.innerHTML = templates.listBundles({bundles});
+
+  mainElement.innerHTML = templates.addBundleForm() + templates.listBundles({ bundles });
+
+  const form = mainElement.querySelector('form');
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const name = form.querySelector('input').value;
+    addBundle(name);
+  });
 };
 
 
@@ -43,7 +78,7 @@ const showView = async () => {
       break;
     case '#list-bundles':
       const bundles = await getBundles();
-      console.log("bundle:",bundles);
+      console.log("bundle:", bundles);
       listBundles(bundles);
       break;
     default:
